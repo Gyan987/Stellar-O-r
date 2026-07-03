73d9598b35aaa731798373d485b1830dac0ee33e
Gyan <gyanprakasht987@gmail.com>
2026-07-03 21:31:07 +0530

    pescript-eslint/no-explicit-any

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
+      
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'react-refresh/only-export-components': 'off'
